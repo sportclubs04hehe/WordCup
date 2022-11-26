a41963099f1437104c228b8a526eb1bc83c09fbc
sportclubs04hehe <vegakinvietnam@gmail.com>
2022-11-26 09:55:58 +0700

    WorldCup2022
    LeMinhHuy

---
+++ WorldCup/WorldCupweb/Controllers/schedule_wc.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorldCupweb.Controllers
+{
+    public class schedule_wc : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Result(int doi1, int doi2, int matran)
+        {
+            ViewData["doi1"] = "result  :" + doi1;
+            ViewData["doi2"] =  doi2;
+            ViewData["matran"] = matran;
+            return View();
+        }
+        
+    }
+}
